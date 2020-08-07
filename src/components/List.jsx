20e34af98bb948d0d7f2e5cc03d61d32be6f9be0
Lucas A Pereira <l.alexandre100@gmail.com>
2020-08-07 15:23:54 -0300

    create-app e componente List

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function List() {
+  return (
+    <div>
+      <h1>Minha lista</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
